70685c6d507135c033b7adf9556cf13186dd68e6
Jonathan Grant <jonathanallengrant@gmail.com>
2024-03-03 19:54:12 -0500

    rm replicate

--- backend/llm.py
+++ backend/llm.py
@@ -1,21 +1,16 @@
 """This file is meant to handle business logic of creating and updating chats."""
 import os
-import time
 import enum
 import uuid
-import base64
 import openai
-import requests
 import tiktoken
 import retrying
-import functools
 
 import jonlog
 
 
 logger = jonlog.getLogger()
 openai.api_key = os.environ.get('OPENAI_KEY') or open(os.path.expanduser('~/.openai_key')).read().strip()
-replicate_api_key = os.environ.get('REPLICATE_KEY') or open(os.path.expanduser('~/.replicate_apikey')).read().strip()
 
 
 class ChatLLMModel(enum.Enum):
@@ -91,66 +86,3 @@ class ChatLLM:
     def get_history(self):
         return self._history
 
-
-class Image:
-    SIZE = {
-        "DALLE2_SMALL": "256x256",
-        "DALLE2_MEDIUM": "512x512",
-        "DALLE2_LARGE": "1024x1024",
-        "DALLE3_SQUARE": "1024x1024",
-        "DALLE3_HORIZONTAL": "1024x1024",
-        "FOFR_MEDIUM": "512x512",
-        "FOFR_HORIZONTAL": "960x480",
-    }
-    MODEL = {
-        "2": "dall-e-2",
-        "3": "dall-e-3",
-        "fofr": "fofr",
-    }
-
-    @classmethod
-    def create(cls, prompt, n=1, response_format="url", model=MODEL["fofr"], size=SIZE["FOFR_MEDIUM"]):
-        logger.info(f'requesting Image with prompt={prompt}, n={n}, response_format={response_format}, model={model}, size={size}...')
-        if model.startswith("dall-e"):
-            resp = openai.OpenAI(api_key=openai.api_key).images.generate(prompt=prompt, n=n, size=size, model=model, response_format=response_format, timeout=45)
-            resp = resp.data
-        else:
-            width, height = size.split('x')
-            width, height = int(width), int(height)
-            resp = requests.post(
-                "https://api.replicate.com/v1/predictions",
-                headers={"Content-Type": "application/json", "Authorization": f"Token {replicate_api_key}"},
-                json={"version": "a83d4056c205f4f62ae2d19f73b04881db59ce8b81154d314dd34ab7babaa0f1", "input": {
-                    "prompt": prompt,
-                    "width": width, "height": height,
-                    "num_images": n,
-                }},
-            )
-            resp = resp.json()
-            sleeps = 0
-            while resp.get("status", "fail").lower() not in {"fail", "succeeded"}:
-                if sleeps >= 10:
-                    raise Exception('Error generating image', resp)
-                logger.info(f"Sleeping 1...")
-                time.sleep(1)
-                sleeps += 1
-                resp = requests.get(f"https://api.replicate.com/v1/predictions/{resp['id']}", headers={"Content-Type": "application/json", "Authorization": f"Token {replicate_api_key}"})
-                resp = resp.json()
-                
-        logger.info('received Image...')
-        return resp
-class FastImage:
-
-    @classmethod
-    def generate(cls, prompt, size='768x512', **kwargs):
-        data = Image.create(prompt, model='fofr', size=size, **kwargs)
-        url = data['output'][0]
-        # Get the MIME type of the image (e.g., 'image/jpeg', 'image/png')
-        mime_type = f"image/{url.rsplit('.', 1)[-1]}"
-        # Encode to base64 and format as a data URI
-        return f"data:{mime_type};base64," + base64.b64encode(requests.get(url).content).decode()
-
-    @classmethod
-    @functools.lru_cache(maxsize=120)
-    def generate_food(cls, dish_name, prompt_template='DSLR photo of {dish_name} served in a restaurant, award-winning, realistic, delicious, 8k resolution'):
-        return cls.generate(prompt_template.format(dish_name=dish_name))
